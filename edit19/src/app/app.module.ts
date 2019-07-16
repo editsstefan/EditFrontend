b8bd464b25e5095b56c16acead1ae637c2cd7616
Stefan <stefan.sakan@gmail.com>
2019-07-16 13:40:23 +0200

    nista ne radi

--- edit19/src/app/app.module.ts
+++ edit19/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StefanComponent } from './stefan/stefan.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StefanComponent,
+    
   ],
   imports: [
     BrowserModule,
---
+++ edit19/src/app/stefan/stefan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stefan',
+  templateUrl: './stefan.component.html',
+  styleUrls: ['./stefan.component.css']
+})
+export class StefanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
